c65cd94672b8e6694b3932b8c4b7518acef546a6
Dimitar Ivanov <dimitucha2004@gmail.com>
2025-09-23 11:24:28 +0300

    Render dynamic movies on home page

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,9 +1,12 @@
 import { Router } from "express";
+import movieService from "../services/movieService.js";
 
 const homeController = Router();
 
 homeController.get("/", (req, res) => {
-  res.render("home");
+  const movies = movieService.getAll();
+  
+  res.render("home", { movies });
 });
 
 homeController.get("/about", (req, res) => {
