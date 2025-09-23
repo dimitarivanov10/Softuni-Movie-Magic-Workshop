b4d12b45ed61332ca9aa26a76c3dd9ccac1b6c91
Dimitar Ivanov <dimitucha2004@gmail.com>
2025-09-23 12:01:55 +0300

    Add movie controller

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import handlebars from "express-handlebars";
 import homeController from "./controllers/homeController.js";
+import movieController from "./controllers/movieController.js";
 const app = express();
 
 //Setup handlebars
@@ -19,6 +20,7 @@ app.use(express.static("src/public"));
 
 //Routes
 app.use(homeController);
+app.use(movieController);
 
 //Start server
 app.listen(5000, () =>
