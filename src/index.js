146e549b8685299189ea1cf611256cb3cb0a978f
Dimitar Ivanov <dimitucha2004@gmail.com>
2025-09-23 08:49:23 +0300

    Add express server + fix in the package.json

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
-console.log("Working");
+import express from "express";
+
+const app = express();
+
+app.get("/", (req, res)=>{
+    res.send("Working 100%");
+})
+
+app.listen(5000, ()=>console.log("Server listening on http://localhost 5000..."));
+
