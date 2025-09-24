65da8aa8437b9336fe63fd2d1fd3e253a6ca7037
Dimitar Ivanov <dimitucha2004@gmail.com>
2025-09-24 10:11:55 +0300

    Add findOne movie as concept and model

--- src/controllers/movieController.js
+++ src/controllers/movieController.js
@@ -15,8 +15,10 @@ movieController.post("/create", (req, res) => {
 
 movieController.get("/:movieId/details", (req, res) => {
   const movieId = req.params.movieId;
+  const movie = movieService.getOne(movieId);
 
   console.log(movieId);
+  console.log(movie);
   res.end();
 });
 
--- src/services/movieService.js
+++ src/services/movieService.js
@@ -4,6 +4,11 @@ export default {
   getAll() {
     return Movie.find();
   },
+  
+  getOne(movieId){
+    return Movie.findOne({_id: movieId});
+  },
+  
   create(movieData) {
     const movie = new Movie(movieData);
     return movie.save();
