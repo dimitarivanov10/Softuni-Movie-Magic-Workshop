bc2e3db66bbb28813d64397346b7d25417d6c986
Dimitar Ivanov <dimitucha2004@gmail.com>
2025-09-24 14:51:16 +0300

    Show search page

--- src/controllers/movieController.js
+++ src/controllers/movieController.js
@@ -20,4 +20,7 @@ movieController.get("/:movieId/details", (req, res) => {
   res.render("details", { movie });
 });
 
+movieController.get("/search", (req, res) => {
+  res.render("search");
+});
 export default movieController;
