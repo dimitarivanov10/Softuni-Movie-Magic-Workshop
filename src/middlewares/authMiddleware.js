d415412b1058dcb6bd86d0736ba34f2b80971870
Dimitar Ivanov <dimitucha2004@gmail.com>
2025-10-09 17:38:37 +0300

    Add dynamic navigation

--- src/middlewares/authMiddleware.js
+++ src/middlewares/authMiddleware.js
@@ -12,6 +12,9 @@ export default function authMiddleware(req, res, next){
         const decodedToken = jwt.verify(token, JWT_SECRET);
         req.user = decodedToken;
         req.isAuthenticated = true;
+
+        res.locals.isAuthenticated = true;
+        res.locals.user = decodedToken;
         next();
     } catch (error) {
         res.clearCookie("auth");
@@ -22,7 +25,7 @@ export default function authMiddleware(req, res, next){
 export function isAuth(req, res, next){
     if(!req.isAuthenticated){
         return res.redirect("/auth/login");
-    }
+    } 
     next();
 }
 
