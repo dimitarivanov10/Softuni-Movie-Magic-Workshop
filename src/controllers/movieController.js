7ea225b9c4df967e94dd6e94cff91a835f1f33d4
Dimitar Ivanov <dimitucha2004@gmail.com>
2025-10-09 17:43:12 +0300

    Move movie related views to their own folder

--- src/controllers/movieController.js
+++ src/controllers/movieController.js
@@ -6,12 +6,10 @@ import { isAuth } from "../middlewares/authMiddleware.js";
 const movieController = Router();
 
 movieController.get("/create", isAuth, (req, res) => {
-
-
-  res.render("create", { pageTitle: "Create Page" });
+  res.render("movies/create", { pageTitle: "Create Page" });
 });
 
-movieController.post("/create", isAuth , async (req, res) => {
+movieController.post("/create", isAuth, async (req, res) => {
   const movieData = req.body;
   await movieService.create(movieData);
   res.redirect("/");
@@ -22,7 +20,7 @@ movieController.get("/:movieId/details", async (req, res) => {
   const movie = await movieService.getOneDetailed(movieId);
 
   const ratingViewData = "&#x2605;".repeat(Math.trunc(movie.rating));
-  res.render("details", {
+  res.render("movies/details", {
     movie,
     pageTitle: "Details Page",
     rating: ratingViewData,
@@ -40,7 +38,7 @@ movieController.get("/search", async (req, res) => {
 movieController.get("/:movieId/attach", async (req, res) => {
   const movieId = req.params.movieId;
   const movie = await movieService.getOne(movieId);
-  const casts = await castService.getAll({excludes: movie.casts});
+  const casts = await castService.getAll({ excludes: movie.casts });
 
   res.render("casts/attach", { movie, casts });
 });
