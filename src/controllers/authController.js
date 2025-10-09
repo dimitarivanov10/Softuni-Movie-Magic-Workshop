76cf26160568349ca8c4e81780727bcb6c73bb4a
Dimitar Ivanov <dimitucha2004@gmail.com>
2025-10-09 17:29:51 +0300

    add isGuest route guard

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
 import authService from "../services/authService.js";
+import { isAuth, isGuest } from "../middlewares/authMiddleware.js";
 
 const authController = Router();
 
-authController.get("/register", (req, res) => {
+authController.get("/register", isGuest, (req, res) => {
   res.render("auth/register");
 });
 
@@ -13,11 +14,11 @@ authController.post("/register", async (req, res) => {
   res.redirect("/auth/login");
 });
 
-authController.get("/login", (req, res) => {
+authController.get("/login", isGuest, (req, res) => {
   res.render("auth/login");
 });
 
-authController.post("/login", async (req, res) => {
+authController.post("/login", isGuest, async (req, res) => {
   const { email, password } = req.body;
   const token = await authService.login(email, password);
 
@@ -26,7 +27,7 @@ authController.post("/login", async (req, res) => {
   res.redirect("/");
 });
 
-authController.get("/logout", (req, res) => {
+authController.get("/logout", isAuth, (req, res) => {
   res.clearCookie("auth");
 
   res.redirect("/");
