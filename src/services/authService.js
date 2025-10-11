758f3d9213fef3350a51e5f303797e3b79bbba13
Dimitar Ivanov <dimitucha2004@gmail.com>
2025-10-11 17:18:57 +0300

    Automatic login on registration

--- src/services/authService.js
+++ src/services/authService.js
@@ -1,12 +1,13 @@
 import User from "../models/User.js";
 import bcrypt from "bcrypt";
-import jwt from "jsonwebtoken";
-
-import { JWT_SECRET } from "../config/constants.js";
+import { generateAuthToken } from "../utils/tokenUtils.js";
 
 export default {
-  register(userData) {
-    return User.create(userData);
+  async register(userData) {
+    const user = await User.create(userData);
+    const token = generateAuthToken(user);
+
+    return token;
   },
   async login(email, password) {
     const user = await User.findOne({ email });
@@ -20,13 +21,7 @@ export default {
     if (!isValid) {
       throw new Error("Invalid username or password!");
     }
-
-    const payload = {
-      id: user.id,
-      email: user.email,
-    };
-
-    const token = jwt.sign(payload, JWT_SECRET, { expiresIn: "2h" });
+    const token = generateAuthToken(user)
     return token;
   },
 };
